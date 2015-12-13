fc19124846c74eed714fa8a7d220c5d1d3618643
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-12-13 15:29:05 -0800

    Add src files with annotations
    Adds two src files which comments indicating the purpose of the
    files as well as some information on the files they contain.

---
+++ src/pca.py
@@ -0,0 +1,11 @@
+# pca.py
+# Contains the functions to create a PCA subspace and represent a vector in that subspace
+
+
+# create_subspace: finds the k principle components of a matrix M and saves them to file
+
+
+# project_upon: projects vector y onto subspace W
+
+
+# load_subspace: returns subspace W from input file in string
