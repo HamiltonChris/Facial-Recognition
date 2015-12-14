Fix errors in create_subspace and add normalization
Fixes some errors in create_subspace that relate to mismatched
matrix dimensions. Also it now normalized the eigenvectors so that
they are orthonormal (unit length ||v|| = 1).

src/pca.py
@@ -3,14 +3,14 @@
 import numpy as np
 from numpy import linalg as la
 
-# create_subspace: finds the k principle components of a matrix M and saves them to file
-def create_subspace(M, k, filename):
-    [images, size] = M.shape 
+# create_subspace: finds the k principle components of a matrix M 
+# returns eigenvalues, eigenvectors, mean
+def create_subspace(M, k):
+    [size, images] = M.shape 
     # calculate the mean 
-    mean = M * (np.ones((images,), dtype=np.int)/ images).T
-    
+    mean = np.dot(M,np.ones((images,1), dtype=np.int))/ images
     if (images > size):
-        covariance = np.dot((M - mean).T, (M - mean))
+        covariance = np.dot((M - mean), (M - mean).T)
         [eigenvectors, eigenvalues] = la.eigh(covariance)
 
     # this should usually be the case since the number of pixels in a picture is probably 
@@ -18,13 +18,18 @@ def create_subspace(M, k, filename):
     # matrix which can be very large we instead calculate the eigenvectors of NxN matrix
     # and then use this to calculate the N eigenvectors of the DxD sized matrix
     else:
-        L = np.dot((M - mean), (M - mean).T) 
-        [eigenvectors, eigenvalues] = la.eigh(L)
-        eigenvectors = np.dot((M - mean).T, eigenvectors)
+        L = np.dot((M - mean).T, (M - mean)) 
+        [eigenvalues, eigenvectors] = la.eigh(L)
+        eigenvectors = np.dot((M - mean), eigenvectors)
     # wow python no scoping in loops, it's kinda hard to take you serious as a language sometimes
+
+    # to make the eigenvectors unit length or orthonormal
+    for i in range(images):
+        eigenvectors[:,i] = eigenvectors[:,i] / la.norm(eigenvectors[:,i])
 
     sorted_order = np.argsort(eigenvalues)
     sorted_order = np.flipud(sorted_order)
+
     eigenvalues = eigenvalues[sorted_order]
     eigenvectors = eigenvectors[:,sorted_order]
 
@@ -37,6 +42,8 @@ def create_subspace(M, k, filename):
         
 
 # project_upon: projects vector y onto subspace W
+
+# save_subspace: saves a subspace to a text file
 
 
 # load_subspace: returns subspace W from input file in string