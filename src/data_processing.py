1847ede0f51d40846bc93735a44d77194a7ec9b7
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-12-14 02:42:50 -0800

    Add functions to load images, flatten and unflatten
    Adds the function load_images which searches the directory and all
    subdirectories for images and formats the images into a flatten
    vector and appends them to an array. Adds flatten_image which makes
    and image matrix a 1xN vector. Adds Unflatten which turns a 1xN
    vector back into a image.

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,10 +1,39 @@
 # data_processing.py 
 # used to turn image data into an appropriate form and vice versa
 
-# flatten_image: turns a image into a 1xN vector
+import os
+import numpy as np
+import cv2
 
+path = '../data/'
+
+# flatten_image: turns a image into a 1xN vector
+def flatten_image(img):
+    # flatten in row major order
+    return img.flatten(order = 'C')
 
 # unflatten_image: turns a 1xN vector into an image
-
+def unflatten_image(vector, rows, columns):
+    # must traverse in row-major order 
+    img = []
+    for i in range(rows):
+        row = vector[i*columns:(i + 1)*columns]
+        img.append(row)
+    image = np.array(img)
+    return image
 
 # load_images: loads images from data set
+# TODO: may need to add inputs to set width and height of images so that
+#       they are uniform
+def load_images(dataset):
+    data_path = path + dataset 
+    X = [] 
+    for root, dirs, files in os.walk(data_path, topdown=False):
+        for name in files:
+            img = cv2.imread(os.path.join(root, name),0)
+            if img is None:
+                continue
+            X.append(flatten_image(img).T) 
+    
+    M = np.array(X).T
+    return M
