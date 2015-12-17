3dbcbb28fe23ad38448ea0aa6280fc2314214ac3
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-12-16 21:57:29 -0800

    Add load single image function
    Adds function to load a single image and formatting it into a flat
    vector for purposes of transforming it into feature space. Adds the
    image loading as greyscale for both load_image and load_images. Fix
    minor typos in display_image.

--- src/utilities.py
+++ src/utilities.py
@@ -24,22 +24,42 @@ def unflatten_image(vector, rows, columns):
 
 # displays input file image
 def display_image(image):
-    cv2.imshow('image',img)
+    cv2.imshow('image',image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 # saves image in given filename 
 def save_image(filename, image):
-    cv2.imwrite(filename, image)
+    image_path = path + filename
+    cv2.imwrite(image_path, image)
 
-# load_images: loads images from data set and formats them to input rows and columns
-#               outputs the formated images as a matrix of flat image vectors
+# loads a saved image called filename and formats it to specified rows and columns
+# (same size if not specified) and outputs as a flatvector
+def load_image(filename, rows=0, columns=0):
+    image_path = path + filename
+    img = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        print image_path + " doesn't contain an image."
+        return 
+    # consider refactoring the below into a function
+    height, width = img.shape
+    if  not rows is 0 and rows < height:
+        top = np.floor((height - rows) / 2)
+        img = img[top:(rows + top),:]
+    if not columns is 0 and columns < width:
+        left = np.floor((width - columns) / 2)
+        img = img[:,left:(columns + left)]
+    return flatten_image(img).T
+
+
+# load_images: loads images as greyscale from data set and formats them to input 
+# rows and columns outputs the formated images as a matrix of flat image vectors
 def load_images(dataset, rows=0, columns=0):
     data_path = path + dataset 
     X = [] 
     for root, dirs, files in os.walk(data_path, topdown=False):
         for name in files:
-            img = cv2.imread(os.path.join(root, name),0)
+            img = cv2.imread(os.path.join(root, name),cv2.IMREAD_GRAYSCALE)
             if img is None:
                 continue
             height, width = img.shape
